1cc93d6317b8a3bdf152c7e4d2223cf9f48cd917
RozoMartinez <111083311+RozoMartinez@users.noreply.github.com>
2022-09-07 19:57:28 -0500

    Creando carpetass

---
+++ src/main/java/Otroproyecto/ejemplodelciclo3/Ejemplodelciclo3Application.java
@@ -0,0 +1,13 @@
+package Otroproyecto.ejemplodelciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplodelciclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplodelciclo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/Otroproyecto/ejemplodelciclo3/Ejemplodelciclo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package Otroproyecto.ejemplodelciclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplodelciclo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
