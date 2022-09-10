f2512023a24a2422d9f5137dd175415ff5a049d1
RozoMartinez <111083311+RozoMartinez@users.noreply.github.com>
2022-09-09 20:23:00 -0500

    Modificación y arreglo

--- src/main/java/Otroproyecto/ejemplodelciclo3/Ejemplodelciclo3Application.java
+++ src/main/java/Otroproyecto/ejemplodelciclo3/Ejemplodelciclo3Application.java
@@ -2,11 +2,6 @@ package Otroproyecto.ejemplodelciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-
-@SpringBootApplication
-public class Ejemplodelciclo3Application {
-=======
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class Ejemplodelciclo3Application {
 	@GetMapping("/Hello")
-	public String hello(){
-		return "Hola este es tu primer comentario";
+	public String Hello (){
+		return "Hola tu priemer comentario";
 	}
->>>>>>> 758d993 (Hola mundo!)
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ejemplodelciclo3Application.class, args);
